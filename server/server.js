2233c1cca044c2d3d16af55e6a1b452ab3f9a49f
John Doe <dev.adipurnomo@gmail.com>
2024-04-20 17:36:33 +0700

    fix: remove unused variable and set default port

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const InputError = require("../exceptions/InputError");
 
 (async () => {
   const server = Hapi.server({
-    port: process.env.PORT,
+    port: process.env.PORT || 8080,
     host: '0.0.0.0',
     routes: {
       cors: {
